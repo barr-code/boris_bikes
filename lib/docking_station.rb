bfab82e215adef786df164d55301422802c53869
Victoria Barr <vb329@georgetown.edu>
2014-09-17 12:45:57 +0100

    Docking Station knows when it's completely empty and when it has no available bikes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -7,4 +7,13 @@ class DockingStation
 	def initialize(options = {})
 		self.capacity = options.fetch(:capacity, capacity)
 	end
+
+	def empty?
+		bike_count == 0
+	end
+
+	def has_rentals?
+		!available_bikes.empty?
+	end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,4 +7,16 @@ describe DockingStation do
 	it "should allow setting default capacity" do
 		expect(station.capacity).to eq(20)
 	end
+
+	it "should know when there are no bikes" do
+		expect(station.bike_count).to eq(0)
+		expect(station).to be_empty
+	end
+
+	it "should know when there are no available bikes" do
+		expect(station.available_bikes).to be_empty
+		expect(station).not_to have_rentals
+	end
+
+	
 end
